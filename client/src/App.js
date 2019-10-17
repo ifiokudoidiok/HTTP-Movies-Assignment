9bfe40ff64795b7b134ef8925426bf28c1fd805a
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-17 16:34:19 +0100

    brought state to top level and implemented PUT and DELETE

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
 import React, { useState } from "react";
+import axios from 'axios';
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
+import UpdateForm from "../src/Movies/UpdateForm";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 
-const App = () => {
+const App = props => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
-
+  
+  const deleteMovie = (id) => (e) => {
+  
+      
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+      .then(({ data }) => {
+        
+        setMovies(movies.filter(movie => movie.id !== data));
+       window.location.href = "/";
+      })
+      .catch(err => console.log(err));
+ 
+}
+  
+  
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      <Route
+        exact
+        path="/"
+        render={props => {
+          return <MovieList {...props} movies={movies} setMovies={setMovies} />;
+        }}
+      />
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return <Movie {...props} addToSavedList={addToSavedList} deleteMovie={deleteMovie}/>;
         }}
+      />
+      <Route
+        path="/update-movie/:id"
+        render={props => (
+          <UpdateForm {...props} movieDetails={movies} setMovies={setMovies} />
+        )}
       />
     </>
   );
